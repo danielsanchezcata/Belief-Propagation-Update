aa9737247f7d89d9f4e3ba131d246a03b1201c8c
danielsanchezcata <92822146+danielsanchezcata@users.noreply.github.com>
2024-10-10 13:07:40 +0100

    Update arbKernel.py to include alpha and beta

--- arbKernel.py
+++ arbKernel.py
@@ -8,6 +8,7 @@ array_double = np.ctypeslib.ndpointer(dtype=np.double,ndim=1, flags='CONTIGUOUS'
 
 lib = ctypes.cdll.LoadLibrary("./out/arbKernel.so")
 
+# Update the argument types to reflect the inclusion of alpha
 lib.full_algorithm.argtypes = [
         ctypes.c_int,
         ctypes.c_int,
@@ -15,7 +16,11 @@ lib.full_algorithm.argtypes = [
         ctypes.c_int,
         ctypes.c_double,
         ctypes.c_double,
-        array_double, array_double,ctypes.c_int]
+        ctypes.c_double,
+        array_double,
+        array_double,
+        ctypes.c_int
+]
 lib.full_algorithm.restype = ctypes.c_int 
 
 lib.compute_Cheb_pts.argtypes = [ array_double, ctypes.c_int ]
@@ -26,23 +31,26 @@ def ChebPts(L):
     lib.compute_Cheb_pts(ans,L)
     return ans
 
-def BP(G, L, beta, tol=10e-10,sm=0):
+def BP(G, L, alpha, beta, tol=10e-10, sm=0):
     M = np.array(list(G.edges())).flatten().astype(ctypes.c_int)
-    output = np.zeros(L*(G.number_of_nodes()+1) + 2)
+    output = np.zeros(L * (G.number_of_nodes() + 1) + 2)
     edgescores = np.zeros(G.number_of_edges())
 
+    # Updated to pass both alpha and beta
     s = lib.full_algorithm(
         ctypes.c_int(G.number_of_nodes()),
         ctypes.c_int(G.number_of_edges()),
         M,
         ctypes.c_int(L),
-        ctypes.c_double(beta),
+        ctypes.c_double(alpha),  # Pass alpha
+        ctypes.c_double(beta),   # Pass beta
         ctypes.c_double(tol),
-        output, edgescores,ctypes.c_int(sm))
+        output, edgescores,
+        ctypes.c_int(sm)
+    )
 
     pts = output[:L].copy()
-    A = output[L:-2].reshape(G.number_of_nodes(),L)
+    A = output[L:-2].reshape(G.number_of_nodes(), L)
     S = output[-2]
     lnZ = output[-1]
-    return pts, A, S, lnZ 
-
+    return pts, A, S, lnZ
